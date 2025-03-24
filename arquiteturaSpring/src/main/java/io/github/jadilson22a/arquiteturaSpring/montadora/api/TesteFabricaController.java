b2de44086e8c52c13fd4e96e4a18a09a2b229a02
Jadilson22a <jadilsonalves777.ma@outlook.com>
2025-03-24 05:58:07 -0300

    Criando annotations com qualifiers

--- arquiteturaSpring/src/main/java/io/github/jadilson22a/arquiteturaSpring/montadora/api/TesteFabricaController.java
+++ arquiteturaSpring/src/main/java/io/github/jadilson22a/arquiteturaSpring/montadora/api/TesteFabricaController.java
@@ -1,7 +1,11 @@
 package io.github.jadilson22a.arquiteturaSpring.montadora.api;
 
 import io.github.jadilson22a.arquiteturaSpring.montadora.*;
+import io.github.jadilson22a.arquiteturaSpring.montadora.configuration.Aspirado;
+import io.github.jadilson22a.arquiteturaSpring.montadora.configuration.Eletrico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +15,7 @@ public class TesteFabricaController {
 
     //Injetamos o bean motor
     @Autowired
+    @Eletrico
     private Motor motor;
 
     @PostMapping
--- arquiteturaSpring/src/main/java/io/github/jadilson22a/arquiteturaSpring/montadora/configuration/MontadoraConfiguration.java
+++ arquiteturaSpring/src/main/java/io/github/jadilson22a/arquiteturaSpring/montadora/configuration/MontadoraConfiguration.java
@@ -4,18 +4,42 @@ import io.github.jadilson22a.arquiteturaSpring.montadora.Motor;
 import io.github.jadilson22a.arquiteturaSpring.montadora.TipoMotor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class MontadoraConfiguration {
 
-    @Bean
-    public Motor motor() {
+    @Bean(name= "motorAspirado")
+    @Primary
+    public Motor motorAspirado() {
         var motor = new Motor();
         motor.setModelo("XPTO-0");
         motor.setCavalos(120);
         motor.setCilindros(4);
         motor.setLitragem(2.0);
         motor.setTipoMotor(TipoMotor.ASPIRADO);
+        return motor;
+    }
+
+    @Bean(name= "motorEletrico")
+    public Motor motorEletrico() {
+        var motor = new Motor();
+        motor.setModelo("Elétrico");
+        motor.setCavalos(120);
+        motor.setCilindros(4);
+        motor.setLitragem(1.4);
+        motor.setTipoMotor(TipoMotor.ELETRICO);
+        return motor;
+    }
+
+    @Bean(name= "motorTurbo")
+    public Motor motorTurbo() {
+        var motor = new Motor();
+        motor.setModelo("Turbo");
+        motor.setCavalos(200);
+        motor.setCilindros(4);
+        motor.setLitragem(2.5);
+        motor.setTipoMotor(TipoMotor.TURBO);
         return motor;
     }
 
